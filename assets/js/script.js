4543f8c83d1769eef44d7d6e0b1367673dcea5c6
Tegaomare <tegaomare@oghenetegasimac.mynetworksettings.com>
2023-05-24 19:08:35 -0400

    added functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,107 @@ saveBtn.addEventListener("click", saveScore);
 playAgainBtn.addEventListener("click", playAgain);
 quitBtn.addEventListener("click", quitGame);
 
+//FUNCTIONS
+function startQuiz() {
+    startBtn.style.display = "none";
+    timerInterval = setInterval(updateTimer, 1000);
+    displayQuestion();
+  }
+
+  function displayQuestion() {
+    var currentQuestion = questions[questionIndex];
+    questionEl.textContent = currentQuestion.question;
+    choicesEl.innerHTML = "";
+    feedbackEl.textContent = "";
+
+    for (var i = 0; i < currentQuestion.choices.length; i++) {
+      var choice = currentQuestion.choices[i];
+      var li = document.createElement("li");
+      li.textContent = choice;
+      choicesEl.appendChild(li);
+    }
+  }
+
+  function answerQuestion(event) {
+    if (event.target.matches("li")) {
+      var selectedAnswer = event.target.textContent;
+      var currentQuestion = questions[questionIndex];
+
+      if (selectedAnswer === currentQuestion.answer) {
+        // Correct answer
+        // Proceed to the next question
+        questionIndex++;
+        if (questionIndex < questions.length) {
+          displayQuestion();
+        }else {
+          // Quiz finished
+          gameOver();
+        }
+      } else {
+        // Incorrect answer
+        timeLeft -= 10; // Deduct 10 seconds from the timer
+        feedbackEl.textContent = "Incorrect!";
+      }
+    }
+  }
+
+  function updateTimer() {
+    timeLeft--;
+    timeLeftEl.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      // Timer reached 0
+      gameOver();
+    }
+  }
+
+  function gameOver() {
+    clearInterval(timerInterval);
+    questionEl.textContent = "";
+    choicesEl.innerHTML = "";
+    feedbackEl.textContent = "";
+    gameOverEl.style.display = "block";
+    displayHighScores();
+  }
+
+  function saveScore() {
+    var initials = initialsInput.value;
+    var score = timeLeft;
+
+    // Save the initials and score
+    var player = { initials: initials, score: score };
+    highScores.push(player);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5); // Keep only the top 5 scores
+
+    // Clear input field
+    initialsInput.value = "";
+
+    // Update the high score board
+    displayHighScores();
+  }
+
+  function displayHighScores() {
+    highScoresEl.innerHTML = "";
+
+    for (var i = 0; i < highScores.length; i++) {
+      var player = highScores[i];
+      var li = document.createElement("li");
+      li.textContent = player.initials + " - " + player.score;
+      highScoresEl.appendChild(li);
+    }
+  }
+
+  function playAgain() {
+    questionIndex = 0;
+    timeLeft = 60;
+    gameOverEl.style.display = "none";
+    startQuiz();
+  }
+
+  function quitGame() {
+    questionIndex = 0;
+    timeLeft = 60;
+    gameOverEl.style.display = "none";
+    startBtn.style.display = "block";
+  }
